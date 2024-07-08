f75afeb44e8eb4e1a262684b3513bdfedbf97d87
Kostiantyn Kostiuk <kostyanf14@live.com>
2024-07-08 23:39:59 +0300

    Refactor status
    Signed-off-by: Kostiantyn Kostiuk <kostyanf14@live.com>

--- app/status/__init__.py
+++ app/status/__init__.py
@@ -1,3 +1,6 @@
+from .ats_status import ATSStatus
+from .gpio_status import GPIOStatus
+from .json_status import JSONStatus
 from .status import Status
 
-__all__ = ['Status']
+__all__ = ['Status', 'GPIOStatus', 'ATSStatus', 'JSONStatus']
